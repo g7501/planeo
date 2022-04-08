6fccba46638ebeba9c249343ec42c76e04745bcf
Ahmed Alkhayyat <ahmedkhaldoon7@gmail.com>
2022-04-08 01:52:51 +0100

    + Added Enemey movment + Added Enemy bullet

--- plane/MainPage.xaml.cs
+++ plane/MainPage.xaml.cs
@@ -24,6 +24,7 @@ using System.Drawing;
 using Windows.UI.Xaml.Shapes;
 using Rectangle = Windows.UI.Xaml.Shapes.Rectangle;
 using plane.game;
+using System.Diagnostics;
 
 
 
@@ -40,10 +41,14 @@ namespace plane
 
         List<Bullet> bullets = new List<Bullet>();
         List<Enemy> enemies = new List<Enemy>();
+        List<Bullet> enemyBullets = new List<Bullet>();
         Player player;
-        
+
+        Stopwatch stopwatch = new Stopwatch();
+
         bool a;
         bool d;
+        bool fire;
 
         public MainPage()
         {
@@ -51,7 +56,7 @@ namespace plane
 
             ImageBrush playerSkin = new ImageBrush();
             playerSkin.ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/space.png"));
-            player = new Player("player", 80, 80, playerSkin, new SolidColorBrush(Colors.Red));
+            player = new Player("player", 80, 80, playerSkin, new SolidColorBrush(Colors.Black));
             Canvas.SetTop(player.getRectangle(), 760);
             addGameObject(player);
 
@@ -63,12 +68,8 @@ namespace plane
 
             _timer.Interval = TimeSpan.FromMilliseconds(0.01666666666);
             _timer.Tick += Tick;
-            _timer.Start();
-
-
-            
+            _timer.Start();            
         }
-
         //
         private void addGameObject(GameObject gb)
         {
@@ -138,82 +139,45 @@ namespace plane
                 fire0 = true;
                 */
 
-                Bullet bullet = new Bullet("bullet", 20, 5, new SolidColorBrush(Colors.Red), new SolidColorBrush(Colors.White));
+                Bullet bullet = new Bullet("bullet", 20, 5, new SolidColorBrush(Colors.White), new SolidColorBrush(Colors.Blue));
 
                 //bullet.setY(player.getY());
                 //bullet.setX(player.getX());
                 bullets.Add(bullet);
-                bulletPos(bullet);
-                addGameObject(bullet);
-
-                //butlletcount++;
-
-               /* if (butlletcount == 1)
-                {
-                    fire0 = true;
-                    bulletPos(bullet0);
-                }
-
-                if (butlletcount == 2)
-                {
-                    fire1 = true;
-                    bulletPos(bullet1);
-                }
-
-                if (butlletcount == 3)
-                {
-                    fire2 = true;
-                    bulletPos(bullet2);
-                }
-
-                if (butlletcount == 4)
-                {
-                    fire3 = true;
-                    bulletPos(bullet3);
-                }
-
-                if (butlletcount == 5)
-                {
-                    fire4 = true;
-                    bulletPos(bullet4);
-                }
-
-                if (butlletcount == 6)
-                {
-                    fire5 = true;
-                    bulletPos(bullet5);
-                }
-
-                if (butlletcount == 7)
-                {
-                    fire6 = true;
-                    bulletPos(bullet6);
-                    butlletcount = 0;
-                }*/
-                //I had if butlletcount == 7 do butlletcount = 1;
-                //I said why I am making a delay just remove it and put it in the last one
-                //I forgot if  (butlletcount == 6) it should be 7 but I had 2*6 So I had more delay
-
+                bulletPos(bullet, player.getRectangle());
+                addGameObject(bullet);  
             }
         }
         // This method will help postion the bullet near the player
-        private void bulletPos(Bullet bullet)
+        private void bulletPos(Bullet bullet, Rectangle rectangle)
         {
-            Canvas.SetTop(bullet.getRectangle(), Canvas.GetTop(player.getRectangle()) -bullet.getRectangle().Height);
-            Canvas.SetLeft(bullet.getRectangle(), Canvas.GetLeft(player.getRectangle()) + player.getRectangle().Width / 2);
+            Canvas.SetTop(bullet.getRectangle(), Canvas.GetTop(rectangle) -bullet.getRectangle().Height);
+            Canvas.SetLeft(bullet.getRectangle(), Canvas.GetLeft(rectangle) + rectangle.Width / 2);
         }
 
         //This mehtod will help the bullet move to the top
-        private void moveBullet(Windows.UI.Xaml.Shapes.Rectangle rec)
+        private void moveBullet(Rectangle rec)
         {
             Canvas.SetTop(rec, Canvas.GetTop(rec) - 10);
+        }
+
+        //THis mothod will help the ememy bullet move toword the player
+        private void moveEnemyBullet(Rectangle rec)
+        {
+            Canvas.SetTop(rec, Canvas.GetTop(rec) + 10);
         }
 
         // This is the time of the game
         private void Tick(object sender, object e)
         {
+            //Calling method move to move the bullet to the top
             for (int i = 0; i < bullets.Count; i++)
                 moveBullet(bullets[i].getRectangle());
+
+            for (int i = 0; i < enemyBullets.Count; i++)
+            {
+                moveEnemyBullet(enemyBullets[i].getRectangle());
+            }
 
             // setting the TEXT to bulletcount for montaining
            // txtCount.Text = "count: " + butlletcount;
@@ -233,7 +197,7 @@ namespace plane
             for (int i = 0; i < bullets.Count; i++)
             {
                 for (int y = 0; y < enemies.Count; y++)
-                {
+                {       // Removimg the enemy & the bullet on the collison
                         if (checkCollision(bullets[i].getRectangle(), enemies[y].getRectangle()))
                         {
                             canvas.Children.Remove(enemies[y].getRectangle());
@@ -251,6 +215,8 @@ namespace plane
                     bullets.RemoveAt(i);
                 }
             }
+
+            //Move the enemy to the right and appear from the left. + go down at the end of the rotation
             float plus = 5;
             for (int i = 0; i < enemies.Count; i++)
             {
@@ -263,8 +229,31 @@ namespace plane
                 }
             }
 
+            //Time to make the enemy shoot
+            stopwatch.Start();
+            if(stopwatch.ElapsedMilliseconds < 2000)
+            {
+                
+
+                //Canvas.GetLeft(enemies[0].getRectangle());
+                txtCount.Text = "Time: " + stopwatch.ElapsedMilliseconds;
+
+            }
+
+            if (stopwatch.ElapsedMilliseconds > 6000)
+            {
+                Bullet enemyBullet = new Bullet("enemy_bullet", 20, 5, new SolidColorBrush(Colors.White), new SolidColorBrush(Colors.Red));
+                txtCount.Text = "Time2: " + stopwatch.ElapsedMilliseconds;
+                bulletPos(enemyBullet, enemies[1].getRectangle());
+                enemyBullets.Add(enemyBullet);
+                addGameObject(enemyBullet);
+                stopwatch.Restart();
+            }
+
+
         }
 
+        // Making the enemy
         private void make()
         {
             int left = 800;
@@ -282,21 +271,19 @@ namespace plane
                     Fill = skin,
                 };*/
 
-                Enemy enemy = new Enemy("enemy", 45, 45, skin, new SolidColorBrush(Colors.White));
+                Enemy enemy = new Enemy("enemy", 45, 45, skin, new SolidColorBrush(Colors.Black));
                 enemies.Add(enemy);
 
                 Canvas.SetTop(enemy.getRectangle(), 10);
                 Canvas.SetLeft(enemy.getRectangle(), left);
-                
+
                 canvas.Children.Add(enemy.getRectangle());
 
                 left -= 60;
 
             }
-
-            
         }
-
+        //Check if there is a collision
         public static bool checkCollision(Rectangle e1, Rectangle e2)
         {
             var r1 = e1.ActualWidth / 2;
